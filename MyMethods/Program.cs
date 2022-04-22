30cb003438deab24a18a33f484db0fffa72882e6
Yordan Iliev <borizmey@gmail.com>
2022-04-22 20:08:28 +0300

    add SortNumbersByAscending

--- MyMethods/Program.cs
+++ MyMethods/Program.cs
@@ -14,3 +14,25 @@ static void PrintNombersInRange(int a, int b)
     }   
 }
 
+static void SortNumbersByAscending(int[] a)
+{
+    int temp;
+
+    for (int i = 0; i < a.Length-1; i++)
+    {
+        for (int j = 0; j < a.Length-1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    foreach (var item in a)
+    {
+        Console.WriteLine(item);
+    }
+  
+}
